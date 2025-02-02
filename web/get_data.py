3b9d36a30c76782420be3d3a2024bd4304e29174
Lauy2k <153731209+Lauy2k@users.noreply.github.com>
2025-02-02 13:46:25 +0100

    Create get_data.py

---
+++ web/get_data.py
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "raspberry";
+$dbname = "sensordaten";
+
+// Verbindung herstellen
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verbindung prüfen
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL-Abfrage (korrekte Namen verwenden!)
+$sql = "SELECT id, Rf, sensor_id, Temp, time FROM dht22 ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    // Daten auslesen
+    $row = $result->fetch_assoc();
+    $data = $row;
+} else {
+    $data = array("error" => "Keine Daten gefunden");
+}
+
+$conn->close();
+
+// Daten als JSON ausgeben
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
